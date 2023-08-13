bbc454285fa512947c19ac475a5b936cdd59f85e
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-13 16:49:32 -0700

    Added custom controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { ActivityModule } from './activity/activity.module';
+import { UserAuthModule } from './user-auth/user-auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), MongooseModule.forRoot(process.env.MONGODB_URI)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nsc-events'),
+    UserModule,
+    ActivityModule,
+    UserAuthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
