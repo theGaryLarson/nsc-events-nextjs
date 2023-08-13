9f5a6fe2f1c10896dde0a82a0ad9021c37ed9e7c
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-13 16:44:37 -0700

    set global route prefix to 'api'

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // set this it makes it easier setting up web server
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
